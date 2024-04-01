a287c0a878b321630c4d76d595b833f844581c84
trideepshivam <trideepshivam@gmail.com>
2024-04-01 12:06:44 +0530

    theme text set as theme front color and login register layout is also set.

--- src/Routes/Layouts/LoginRegister/LoginRegisterLayout.jsx
+++ src/Routes/Layouts/LoginRegister/LoginRegisterLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './LoginRegisterLayout.css'
+import logo from '../../../assets/logo-reverse.svg'
  
 const LoginRegisterLayout=()=>{
     return(
         <div className="container">
-            LoginRegisterLayout
-            <br />
+            <img width="50em" src={logo} alt="Logo" />
             <Outlet/>
         </div>
     )
