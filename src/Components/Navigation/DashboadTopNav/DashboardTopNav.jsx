16e43f861bcd2102d9897d35604fdf3095f6b96a
trideepshivam <trideepshivam@gmail.com>
2024-04-03 17:48:01 +0530

    side nav set properly and used state from top nav to open and close it from menu button

--- src/Components/Navigation/DashboadTopNav/DashboardTopNav.jsx
+++ src/Components/Navigation/DashboadTopNav/DashboardTopNav.jsx
@@ -3,18 +3,22 @@ import logo from '../../../assets/logo-reverse.svg'
 import ToggleDarkLight from '../../ToggleDarkLight/ToggleDarkLight';
 import { useContext } from 'react';
 import { Context } from '../../../ContextAPI';
-const DashboardTopNav=()=>{
+
+const DashboardTopNav=({sideNavOpen,setSideNavOpen})=>{
     const {lightMode} = useContext(Context);
     
-    const closebtn = <img width="25" height="25" src={
-        lightMode?"https://img.icons8.com/ios/25/000000/delete-sign--v1.png":
-                "https://img.icons8.com/ios/25/ffffff/delete-sign--v1.png"
-    } alt="close"/>
+    const menu = !sideNavOpen?
+        !lightMode?"https://img.icons8.com/ios-filled/50/000000/menu--v6.png":
+                "https://img.icons8.com/ios-filled/50/ffffff/menu--v6.png":
+        !lightMode?"https://img.icons8.com/ios-filled/25/000000/delete-sign--v1.png":
+                "https://img.icons8.com/ios-filled/25/ffffff/delete-sign--v1.png"
 
     return(
         <div className="topNavigation">
-            <div className='navElement'><h3 className='navMenuButton'>&#9776;</h3></div>
-            <div className='navElement'><img width="50em" src={logo} alt="Logo" /></div>
+            <div className='navElement'>
+                <img onClick={()=>setSideNavOpen(!sideNavOpen)} width="25" height="25" src={menu} alt="menu"/>               
+            </div>
+            <div className='navElement'><img width="40em" src={logo} alt="Logo" /></div>
             <div className='navElement'><ToggleDarkLight/></div>
         </div>
     )
--- src/Routes/Layouts/DashboardLayout/DashboardLayout.jsx
+++ src/Routes/Layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import './DashboardLayout.css'
 import DashboardTopNav from '../../../Components/Navigation/DashboadTopNav/DashboardTopNav';
+import SideNavigation from '../../../Components/Navigation/SideNavigation/SideNavigation';
+import { useState } from 'react';
  
 const DashboardLayout=()=>{
+    const [sideNavOpen,setSideNavOpen]=useState(true)
     return(
         <>
-            <DashboardTopNav/>
+            <DashboardTopNav sideNavOpen={sideNavOpen} setSideNavOpen={setSideNavOpen}/>
             <div className="navContentContainer">
+                <SideNavigation sideNavOpen={sideNavOpen}/>
                 <Outlet/>
             </div>
         </>
