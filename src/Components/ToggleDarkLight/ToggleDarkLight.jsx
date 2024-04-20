a697553725bf82981df48762f887b98996938827
trideepshivam <trideepshivam@gmail.com>
2024-04-20 10:54:25 +0530

    lightmode darkmode set permanently even if user refreshes or close the browser the theme will be set according to the user choice which will be saved into the local storage

--- src/Components/ToggleDarkLight/ToggleDarkLight.jsx
+++ src/Components/ToggleDarkLight/ToggleDarkLight.jsx
@@ -3,23 +3,25 @@ import './ToggleDarkLight.css'
 import { Context } from "../../ContextAPI";
 
 const ToggleDarkLight = () => {
-    const [toggleOn,setToggleOn] = useState(true);//switch on for dark mode defaultly
-    let imgurl = toggleOn?
+    const {lightMode,setLightMode} = useContext(Context);
+    const handleMode=()=>{
+        localStorage.setItem('LIGHT_MODE',!lightMode)
+        setLightMode(!lightMode)
+    }
+    let imgurl = lightMode?
         "https://img.icons8.com/external-glyph-silhouettes-icons-papa-vector/28/FAB005/external-Light-Mode-interface-glyph-silhouettes-icons-papa-vector.png":
         "https://img.icons8.com/sf-regular-filled/28/000000/moon-symbol.png";
-    const {setLightMode} = useContext(Context);
 
     useEffect(()=>{
-        !toggleOn?document.body.classList.add("dark-theme"):document.body.classList.remove("dark-theme");
-        setLightMode(toggleOn);
-    },[toggleOn])
+        !lightMode?document.body.classList.add("dark-theme"):document.body.classList.remove("dark-theme");
+    },[lightMode])
 
     return(
         <div className="toggleContainer">
-            <input id="toggleCheckbox" type="checkbox" onChange={()=>setToggleOn(!toggleOn)}  checked={toggleOn}/>
+            <input id="toggleCheckbox" type="checkbox" onChange={handleMode}  checked={lightMode}/>
             <label htmlFor="toggleCheckbox" className="toggle">
                 <img width="28" height="28" src={imgurl} alt="Mode"/>
-                {!toggleOn?"Dark mode":"Light mode"}
+                {!lightMode?"Dark mode":"Light mode"}
             </label>
         </div>
     )
--- src/ContextAPI.jsx
+++ src/ContextAPI.jsx
@@ -3,12 +3,13 @@ import { createContext, useState } from "react";
 export const Context = createContext()
 
 const ContextAPI = ({children})=>{
-    const [lightMode,setLightMode] = useState(false);
-    const [userDetails,setUserDetails] = useState({
-        id:'1234',
-        email:'xyz@gmail.com',
-        token:'1q2w3e4r5t6y'
-    })//JSON.stringify(localStorage.getItem('USER_DETAILS')));
+    const [lightMode,setLightMode] = useState(
+        localStorage.getItem('LIGHT_MODE')=='false'?false:true
+    );
+    
+    const [userDetails,setUserDetails] = useState(
+        JSON.stringify(localStorage.getItem('USER_DETAILS'))
+    );
     return(
         <Context.Provider value={{
             lightMode,
