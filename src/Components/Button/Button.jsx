32912093451dbef49ba651fdee40aee58ba29ae0
trideepshivam <trideepshivam@gmail.com>
2024-04-27 01:06:49 +0530

    changed button transparancy property added prop to get transparancy true or false

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import './Button.css'
  
 const Button=(props)=>{
+    const btnClass = props.transparancy?'themeButton transparent':'themeButton'
     return(
         <button
             style={props.style&&props.style}
             type='submit'
-            className='themeButton'
+            className={btnClass}
             onClick={props.onClick&&props.onClick}
         >{props.value}</button>
     )
