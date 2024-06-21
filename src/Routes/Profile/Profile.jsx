82b7ca57472803cc653d8ac8991c1ff6b009fc73
shashwat Tejaswi <shashwatteju06@gmail.com>
2024-06-21 11:45:02 +0530

    design profile 80%

--- src/Routes/Profile/Profile.jsx
+++ src/Routes/Profile/Profile.jsx
@@ -1,8 +1,24 @@
 import './Profile.css'
+import Textbox from '../../Components/Textbox/Textbox';
  
 const Profile=()=>{
+    const allTextField=["First Name",'Last Name','Instagram','Twitter','Email']
     return(
+        <div className='bodyBox'>
         <p style={{margin:'0'}} className="sectionHead">PROFILE</p>
+        <div className='containerBox'>
+           <div className='inputImage'>
+                <img height='100px' width='100px' alt='soon'/>
+                <div className='textArea'>
+                   {allTextField.map((a)=><Textbox legend={a}></Textbox>)}
+                </div>
+           </div>
+           <div className='achivement'>
+                <span>ACHIVEMENTS</span>
+                <div className="Achived"></div>
+           </div>
+        </div>
+        </div>
 
     )
 }
