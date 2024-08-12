e2184d648a587f30564ea405a0925288604d2266
shashwat Tejaswi <shashwatteju06@gmail.com>
2024-08-12 23:56:17 +0530

    made admin-Login page

--- src/routes/admin/Admin.jsx
+++ src/routes/admin/Admin.jsx
@@ -1,9 +1,19 @@
+import './Admin.css'
+import Textbox from '../../components/textbox/Textbox';
+import Hyperlink from '../../components/hyperlink/Hyperlink';
+import Button from '../../components/button/Button';
 
 const Admin=()=>{
 
     return(
         <>
-        <p>admin</p>
+        <div className="loginContainer">
+            <h2 className='highlight'>LOGIN-ADMIN</h2>
+            <Textbox  type="text" legend="Email"/>
+            <Textbox  type="Password" legend="Key"/>
+            <Hyperlink href="/forgot-password" value="Forgot Password?"/>
+            <Button  value="Login"/>
+        </div>
         </>
     )
 }
