eff53a43949e3a250775d4a7c70d7ff3f058853c
trideepshivam <trideepshivam@gmail.com>
2024-08-22 15:27:54 +0530

    created language confirmation box to check that before typing start user have changed layout or not

---
+++ src/components/languageConfirmation/LanguageConfirmation.jsx
@@ -0,0 +1,12 @@
+import PopUpContainer from '../popUpContainer/PopUpContainer';
+import './LanguageConfirmation.css'
+ 
+const LanguageConfirmation=()=>{
+    return(
+        <PopUpContainer>
+            <p>confirm box</p>
+        </PopUpContainer>
+    )
+}
+
+export default LanguageConfirmation;
