c488247abf7bd5cd88b886d9fcbcb8c43727572e
trideepshivam <trideepshivam@gmail.com>
2024-09-12 14:42:57 +0530

    added koala gif for loading and changed img into the loading component

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,12 +1,12 @@
 import './Loading.css'
-import loading from '../../assets/loading-mouse.gif'
+import loading from '../../assets/loading-koala.gif'
 
 const Loading =()=>{
 
     return(
         <div className='loadingContainer'>
-            <img src={loading} alt="" />
-            <p>Wait for a while...</p>
+            <img width='150px' src={loading} alt="" />
+            <p className='highlight'>Wait for a while...</p>
         </div>
     )
 }
