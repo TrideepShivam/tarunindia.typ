8caaada7c385e0208cabf2a1674a4f7f6c5a5113
trideepshivam <trideepshivam@gmail.com>
2024-09-12 15:07:21 +0530

    added congo gif to the result container if accuracy is greater than 95

--- src/components/resultDetail/percentage/Percentage.jsx
+++ src/components/resultDetail/percentage/Percentage.jsx
@@ -1,8 +1,8 @@
 import './Percentage.css'
  
 const Percentage=(props)=>{
-
-    const percentageStyle = "conic-gradient(var(--theme-color) "+(props.value*3.6)+"deg, var(--background-color) 0deg)";
+    const percentageColor = props.value<95?'tomato':'var(--theme-color)'
+    const percentageStyle = "conic-gradient("+percentageColor+" "+(props.value*3.6)+"deg, var(--background-color) 0deg)";
     
     return(
         <div className="outer" style={{backgroundImage:percentageStyle}}>
