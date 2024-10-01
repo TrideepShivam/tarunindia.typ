3c12f84e5f6368fa3483148085a0d042fca4b268
trideepshivam <trideepshivam@gmail.com>
2024-10-01 21:58:25 +0530

    created a button like navigation hyperlink with hover effect

--- src/components/navigation/topNavigation/TopNavigation.jsx
+++ src/components/navigation/topNavigation/TopNavigation.jsx
@@ -2,16 +2,17 @@ import { Link } from 'react-router-dom';
 import './TopNavigation.css'
 import logo from '../../../assets/logo-reverse.svg'
 import ToggleDarkLight from '../../toggleDarkLight/ToggleDarkLight';
+import Hyperlink from '../../hyperlink/Hyperlink';
 const TopNavigation=()=>{
-    const arr=[{name:"Home",path:"/",color:false},{name:"Login",path:"/login",color:true},{name:"Register",path:"/register",color:true}];
+    const arr=[{name:"Home",path:"/",color:false},{name:"Register",path:"https://trideepshivam.in",color:true}];
     return(
         <div className="topNavigation">
             <img width="50em" src={logo} alt="Logo" />
-               
-                <div className="navSlab">
-                    {arr.map((a,index)=><Link key={index} className={`${a.color? "bluegoto":"goto"}`} to={a.path}>{a.name}</Link>)}
-                    <ToggleDarkLight/>
-                </div>
+            <div className="navSlab">
+                {arr.map((a,index)=><Link key={index} className={`${a.color? "bluegoto":"goto"}`} to={a.path}>{a.name}</Link>)}
+                <Hyperlink type='buttonLike' href='/login' value="Login"/>
+                <ToggleDarkLight/>
+            </div>
             
         </div>
     )
