e40ffd380d75786ff2f19d859706db6206ad155e
trideepshivam <trideepshivam@gmail.com>
2024-10-06 23:49:58 +0530

    added connected context to show the boolean value if system of online or ofline

--- src/ContextAPI.jsx
+++ src/ContextAPI.jsx
@@ -1,17 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import MsgBox from "./components/msgBox/MsgBox";
 
 export const Context = createContext()
 
 const ContextAPI = ({children})=>{
-    
+    const [connected,setConnected] = useState(navigator.onLine)
     const [msg,setMsg] = useState(false)//for messagebox
     const [responsive,setResponsive]=useState(//for detecting mobile or tab
         navigator.userAgent.search('Mobile')>=0||navigator.userAgent.search('Tablet')>=0
     )
     //setting the resizing event to change the responsive value
-    window.onresize = ()=>window.innerWidth<820?setResponsive(true):setResponsive(false)
     
+    useEffect(() => {
+        window.onresize = () => window.innerWidth < 820 ? setResponsive(true) : setResponsive(false);
+        window.addEventListener('online', () => setConnected(true));
+        window.addEventListener('offline', () => setConnected(false));
+
+        // Cleanup event listeners on component unmount
+        return () => {
+            window.removeEventListener('online', () => setConnected(true));
+            window.removeEventListener('offline', () => setConnected(false));
+        }
+    }, [])
     const [lightMode,setLightMode] = useState(//for light mode dark mode
         localStorage.getItem('LIGHT_MODE')=='false'?false:true
     );
@@ -40,7 +50,8 @@ const ContextAPI = ({children})=>{
             setUserLocal,
             msg,
             setMsg,
-            responsive
+            responsive,
+            connected
         }}>
             {children}
             {msg.isOpen&&<MsgBox setMsg={setMsg} data={msg}/>}
