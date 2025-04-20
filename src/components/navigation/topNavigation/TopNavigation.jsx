91b6e952d867c69b918ac54d3f54d3aae5355887
KumarRituRaj <kumarrituraj2000@gmail.com>
2025-04-20 15:26:26 +0530

    fix: Remove Home link on the home page

--- src/components/navigation/topNavigation/TopNavigation.jsx
+++ src/components/navigation/topNavigation/TopNavigation.jsx
@@ -5,9 +5,13 @@ import Hyperlink from '../../hyperlink/Hyperlink';
 import { useContext, useState } from 'react';
 import { Context } from '../../../ContextAPI';
 import CircleButton from '../../circleButton/CircleButton';
+import { useLocation } from 'react-router-dom';
+
 const TopNavigation = () => {
     const { responsive, lightMode } = useContext(Context);
     const [navOpen, setNavOpen] = useState(false);
+    const location = useLocation();
+
     const closeBtn = (
         <img
             width="25"
@@ -40,15 +44,18 @@ const TopNavigation = () => {
                                 onClick={() => setNavOpen(false)}
                             />
                         )}
-                        {topNavMenu.map((a, index) => (
-                            <Hyperlink
-                                type={a.name == 'Pricing' ? 'bordered-theme' : 'trans-hover'}
-                                key={index}
-                                href={a.path}
-                                value={a.name}
-                                onClick={() => setNavOpen(false)}
-                            />
-                        ))}
+                        {topNavMenu.map((a, index) => {
+                            if (a.path === '/' && location.pathname === '/') return null; // Don't show Home link on the home page
+                            return (
+                                <Hyperlink
+                                    type={a.name === 'Pricing' ? 'bordered-theme' : 'trans-hover'}
+                                    key={index}
+                                    href={a.path}
+                                    value={a.name}
+                                    onClick={() => setNavOpen(false)}
+                                />
+                            );
+                        })}
                         {responsive && (
                             <Hyperlink
                                 type="themed"
