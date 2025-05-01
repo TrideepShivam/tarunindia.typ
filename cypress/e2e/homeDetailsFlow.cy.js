9ece98bc2296a5e40fa68dc60bfa21bf4573f6b6
KumarRituRaj <kumarrituraj2000@gmail.com>
2025-05-02 01:08:34 +0530

    feat: new cypress test added

--- cypress/e2e/homeDetailsFlow.cy.js
+++ cypress/e2e/homeDetailsFlow.cy.js
@@ -80,6 +80,134 @@ describe('All Visible Literals on Home Page', () => {
         '12+ hrs supported team to cover all the inconveniences during app use. Nothing can distract you from your goals.',
     ];
 
+    const priceTexts = [
+        // Main heading
+        'Boost your typing skill',
+        'with Typathon',
+        'Yearly and Half Yearly plan is starting from',
+        '₹5',
+
+        // Free Subscription
+        'Free Subscription',
+        'Unlock a Free Typathon Subscription with Your Typing or Computer Certificate!',
+        '15 days',
+        'full access of everything',
+        'Contact Us',
+
+        // Comparison section
+        'Why Premium',
+        'Compare our Freemium and Premium Plans',
+        '1 min Testing',
+        '10 min Testing',
+        'Advanced Analytics',
+        'Event Tickets',
+        'Exam Based Test',
+        'Schedule Reminder',
+        'Ad-Free Experience',
+        'Priority Updates',
+
+        // Plans
+        'Choose your plan',
+        '365D',
+        '180D',
+        '90D',
+        '30D',
+        '15D',
+        'English',
+        '₹900',
+        '₹2.47',
+        'English access',
+        'Paid Event Tickets',
+        'Krutidev',
+        'Krutidev access',
+        'Mangal',
+        '₹1000',
+        '₹2.74',
+        'Mangal access',
+        'En, Kr, Mn',
+        '₹1500',
+        '₹4.11',
+        'Unlimited access',
+        'Free Event Tickets',
+        '24/7 support',
+        'Buy Now',
+
+        // Bulk subscription
+        'Bulk Subscription',
+        "Empower Your Institution with Typathon's Exclusive Bulk Subscription Plan!",
+        'Exclusive Benefits of Our Bulk Subscription:',
+        'Cost Savings',
+        "Empower Your Institution with Typathon's Exclusive Bulk Subscription Plan!",
+        'Enjoy significant discounts with our bulk subscription rates, making high-quapty training affordable.',
+        'Enhanced Learning Experience',
+        'Provide your students with comprehensive typing skills that enhance both academic and professional performance.',
+        'Dedicated Support',
+        'Gain access to additional resources and priority support to ensure a successful training experience.',
+
+        // FAQ section
+        'Frequently Asked Questions',
+        'What are the available pricing plans?',
+        'Can I cancel my subscription at any time?',
+        'Can I try this before purchasing a plan?',
+        'What features does the Premium plan offer?',
+        'Are there any discounts available?',
+        'What payment methods are accepted?',
+        'How can I contact customer support?',
+
+        // About
+        'Typathon, your go-to platform for enhancing your typing skills!',
+        'Our mission is to provide an engaging and effective way for users of all levels to improve their typing speed and accuracy.',
+        'Whether you’re a beginner just starting out or an experienced typist looking to refine your skills,',
+        'our customizable tests and user-friendly interface make practice enjoyable and productive.',
+        'Join us on your journey to becoming a typing pro!',
+
+        // Footer
+        'ABOUT',
+        'Welcome to Typathon',
+        'typing pro!',
+        'CONTACT',
+        'TARUN INDIA INSTITUTE',
+        'In Front of Police Station, ward no-13,',
+        'Madhepura, Bihar, India',
+        '852113',
+        'info@typathon.com',
+        '9546747447',
+        'terms & services',
+        'privacy policy',
+        'copyright @ 2024',
+    ];
+
+    const faqItems = [
+        {
+            question: 'What are the available pricing plans?',
+            answer: 'Typathon offers several pricing plans based on the language you choose. Each plan comes with different languages and benefits to cater to various user needs.',
+        },
+        {
+            question: 'Can I try this before purchasing a plan?',
+            answer: 'Yes, Typathon offers a free trial for new users to experience the service before committing to a paid plan. In the free plan, you get unlimited experiences of 1 minute test and 1 test per day for 10 minute testing.',
+        },
+        {
+            question: 'What features does the Premium plan offer?',
+            answer: 'The Premium plan includes all the features of the Freemium plan, plus access to advanced typing tests, progress tracking for multiple languages, and priority support. You’ll also get advanced analytics and free event tickets (available only in the all-subjects plan).',
+        },
+        {
+            question: 'Are there any discounts available?',
+            answer: 'Yes, Typathon offers discounts for users who subscribe to annual or 6-month plans. You can enjoy rates as low as ₹5 per day with yearly or half-yearly subscriptions. Please contact our support team for more details.',
+        },
+        {
+            question: 'What payment methods are accepted?',
+            answer: 'Currently, Typathon accepts only manual payment methods. First, chat with us with you registered email id and choose the plan. After payment confirmaion with screenshot enjoy your plan.',
+        },
+        {
+            question: 'Can I cancel my subscription at any time?',
+            answer: 'No, subscriptions cannot be canceled once purchased. However, we offer a Freemium plan to let you try out the platform before making a decision. Choose a plan that best fits your needs after trying the demo.',
+        },
+        {
+            question: 'How can I contact customer support?',
+            answer: 'You can reach our customer support team via email at info@typathon.com or through our WhatsApp chat at 9546747447.',
+        },
+    ];
+
     beforeEach(() => {
         cy.visit('/');
     });
@@ -127,5 +255,30 @@ describe('All Visible Literals on Home Page', () => {
         cy.contains('Back to Home').click();
         cy.url().should('eq', Cypress.config().baseUrl + '/');
         cy.wait(500);
+    });
+
+    it('navigates to /pricing and displays all key sections', () => {
+        cy.contains('Pricing').click();
+        cy.url().should('include', '/pricing');
+        cy.wait(500);
+        priceTexts.forEach((text) => {
+            cy.contains(text).should('exist');
+        });
+
+        // Additional element checks
+        cy.get('table').should('exist');
+        cy.get('.planCard').should('have.length.at.least', 1);
+        cy.get('img[alt="certificate"]').should('exist');
+    });
+
+    it('displays correct answers for each FAQ item when clicked', () => {
+        cy.contains('Pricing').click();
+        cy.url().should('include', '/pricing');
+        cy.wait(500);
+        faqItems.forEach(({ question, answer }) => {
+            cy.contains(question).click();
+            cy.wait(500);
+            cy.contains(answer).should('be.visible');
+        });
     });
 });
